d923d520c6454d318805b6a7376aebecd8693ecb
hjksdnjs <1105600104@qq.com>
2020-04-27 18:59:23 +0800

    user的answerlist

--- src/components/user/answerList.vue
+++ src/components/user/answerList.vue
@@ -22,7 +22,7 @@
 
       <el-table-column
         sortable
-        prop="ans_id"
+        prop="ansId"
         label="回答id"
         header-align="left"
         align="left"
@@ -32,7 +32,7 @@
 
       <el-table-column
         sortable
-        prop="ans_content"
+        prop="ansContent"
         label="回答内容"
         header-align="left"
         align="left"
@@ -42,18 +42,21 @@
 
       <el-table-column
         sortable
-        prop="ans_time"
+        prop="ansTime"
         label="回答发布时间"
         header-align="left"
         align="left"
         :show-overflow-tooltip="true">
+        <template slot-scope="aListData">
+          {{ aListData.row.ansTime | dateFmt('YYYY-MM-DD HH:mm:ss')}}
+        </template>
       </el-table-column>
 
 
 
       <el-table-column
         sortable
-        prop="good_count"
+        prop="goodCount"
         label="赞的个数"
         header-align="left"
         align="left"
@@ -62,7 +65,7 @@
 
       <el-table-column
         sortable
-        prop="bad_count"
+        prop="badCount"
         label="踩的个数"
         header-align="left"
         align="left"
@@ -71,7 +74,7 @@
 
       <el-table-column
         sortable
-        prop="ans_state"
+        prop="ansState"
         label="回答的状态"
         header-align="left"
         align="left"
@@ -102,8 +105,9 @@
 </template>
 
 <script>
+  import qs from 'qs';
   var listJson = {
-    aListData: [{
+    aListData: [/*{
       ans_id:'1',
       ans_content:'内容a',
       ans_time:'2020-03-27 13:07:40',
@@ -126,7 +130,7 @@
         good_count:'1',
         bad_count:'2',
         ans_state:'0',
-      }],
+      }*/],
   }
   export default {
     name: "answerList",
@@ -144,9 +148,6 @@
     created() {
       this.pageList()
     },
-    /*mounted(){
-      this.getaListData();
-    },*/
     methods: {
       pageList() {
         // 发请求拿到数据并暂存全部数据,方便之后操作
@@ -154,6 +155,16 @@
         this.getaListData()
       },
       getaListData:function() {
+        this.$axios.post('http://localhost:8080/online_answer/user/searchAnswersByUserId',
+          qs.stringify({
+            userId: '1',
+          })
+        ).then((response) => {
+          console.log(response.data.data);
+          this.aListData = response.data.data;
+        }).catch((error) => {
+          console.log(error);
+        });
         let aListData = this.data.filter((item,index) =>
           item.ans_content.includes(this.search_input)
         )
