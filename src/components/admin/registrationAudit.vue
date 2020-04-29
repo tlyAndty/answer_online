cd4b8d6e5909cc27124644a9a63d21ef6e1c0f9a
hjksdnjs <1105600104@qq.com>
2020-04-29 11:50:22 +0800

    审核信息页面除“查看详情”之外的操作实现

--- src/components/admin/registrationAudit.vue
+++ src/components/admin/registrationAudit.vue
@@ -64,9 +64,9 @@
           align="center"
           min-width="100">
           <template slot-scope="scope">
-            <el-button type="text" @click="checkDetail(scope.row.phone)">查看详情</el-button>
-            <el-button type="text" @click="acceptUser(scope.row.phone)">通过</el-button>
-            <el-button type="text" @click="rejectUser(scope.row.phone)">拒绝</el-button>
+            <el-button type="text" @click="checkDetail(scope.row.userId)">查看详情</el-button>
+            <el-button type="text" @click="acceptUser(scope.row.userId)">通过</el-button>
+            <el-button type="text" @click="rejectUser(scope.row.userId)">拒绝</el-button>
           </template>
         </el-table-column>
 
@@ -147,9 +147,9 @@
           this.uListData = response.data.data;
           for(let item of this.uListData) {
             if(item.state==0){
-              console.log(item.userId)
+              //console.log(item.userId)
               this.rListData.push(item)
-              console.log(item)
+              //console.log(item)
             }
           }
         }).catch((error) => {
@@ -162,17 +162,6 @@
           index < this.page * this.limit && index >= this.limit * (this.page - 1)
         )
         this.total = rListData.length
-        /*this.$axios.get(
-          '127.0.0.1/online_answer/user/login'
-        ).then(response => {
-          const res = response.data
-          if (res.data) {
-            const data = res.data
-            this.qListData = data.qListData
-          }
-        }).catch(error => {
-          console.log('错误信息：' + error)
-        })*/
       },
       handleSizeChange(val) {
         console.log(`每页 ${val} 条`);
@@ -189,14 +178,33 @@
         this.getrListData()
       },
       rejectUser(val){
-        console.log(val)
-//这里写相应的逻辑，val是指传进来的参数也就是上面的scope.row.phone；也可以是scope.row.nickname等
+        this.$axios.post('http://localhost:8080/online_answer/admin/modifyUserState',
+          qs.stringify({
+            userId: val,
+            userState: '3',
+          })
+        ).then((response) => {
+          console.log(response.data.resultCode)
+          console.log("修改成功")
+        }).catch((error) => {
+          console.log(error);
+        });
       },
       acceptUser(val){
-        let self = this;
+        this.$axios.post('http://localhost:8080/online_answer/admin/modifyUserState',
+          qs.stringify({
+            userId: val,
+            userState: '1',
+          })
+        ).then((response) => {
+          console.log(response.data.resultCode)
+          console.log("修改成功")
+        }).catch((error) => {
+          console.log(error);
+        });
       },
       checkDetail(val){
-        window.location.href='/questionPage'
+        this.$router.push({path:path,query:{user_id:val}})
         console.log(val)
       },
     },
