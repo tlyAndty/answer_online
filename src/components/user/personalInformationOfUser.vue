393c9960ec74cf20a5d4ac43cc14c548d13a116b
hjksdnjs <1105600104@qq.com>
2020-04-30 17:17:01 +0800

    头像 添加页脚

--- src/components/user/personalInformationOfUser.vue
+++ src/components/user/personalInformationOfUser.vue
@@ -123,7 +123,7 @@
       resetForm(formName) {
         this.$refs[formName].resetFields();
       },
-      handleAvatarSuccess(response, file) {
+      handleAvatarSuccess(res, file) {
         this.$axios.post('http://localhost:8080/online_answer/user/upload',
           qs.stringify({
             userId: this.id,
