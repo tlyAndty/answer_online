8882a34072977ff58ae978520204659a4e0df62f
hjksdnjs <1105600104@qq.com>
2020-06-11 23:34:41 +0800

    图表配色

--- src/components/admin/registerChart.vue
+++ src/components/admin/registerChart.vue
@@ -61,33 +61,33 @@
             legend: {               //设置区分（哪条线属于什么）
               data:['注册量']
             },
-            color: ['#8AE09F'],       //设置区分（每条线是什么颜色，和 legend 一一对应）
+            color: ['#f08181'],       //设置区分（每条线是什么颜色，和 legend 一一对应）
             xAxis: {                //设置x轴
               type: 'category',
               boundaryGap: false,     //坐标轴两边不留白
               data: ['2019-1-1', '2019-2-1', '2019-3-1', '2019-4-1', '2019-5-1', '2019-6-1', '2019-7-1',],
               name: '日期',           //X轴 name
               nameTextStyle: {        //坐标轴名称的文字样式
-                color: '#FA6F53',
+                color: '#666',
                 fontSize: 16,
                 padding: [0, 0, 0, 20]
               },
               axisLine: {             //坐标轴轴线相关设置。
                 lineStyle: {
-                  color: '#FA6F53',
+                  color: '#666',
                 }
               }
             },
             yAxis: {
               name: '人数',
               nameTextStyle: {
-                color: '#FA6F53',
+                color: '#666',
                 fontSize: 16,
                 padding: [0, 0, 10, 0]
               },
               axisLine: {
                 lineStyle: {
-                  color: '#FA6F53',
+                  color: '#666',
                 }
               },
               type: 'value'
@@ -99,7 +99,8 @@
                 type: 'line',               // 类型为折线图
                 lineStyle: {                // 线条样式 => 必须使用normal属性
                   normal: {
-                    color: '#8AE09F',
+                    //color: '#bbe6ec',
+                    color: '#f08181',
                   }
                 },
               }
