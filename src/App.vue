ad3dd7494caa7c895d11e01f9f5421b91eaee884
hjksdnjs <1105600104@qq.com>
2020-06-18 15:55:51 +0800

    消息盒子数据实时更新

--- src/App.vue
+++ src/App.vue
@@ -11,33 +11,33 @@
           <router-link style="margin-right: 15px;color: #ffffff;text-decoration:none;position: relative;top: 20px" :to="{name:'userGuide',query:{user_id:user.userId}}">
             <img v-if="imageUrl" :src="imageUrl" class="avatar" style="height:50px;width:50px;border-radius:25px;border:none;">
           </router-link>
-        <el-dropdown trigger="click" style="color: white;margin-right: 10px;font-size: 16px">
-          <span class="el-dropdown-link">
-            消息
-            <i class="el-icon-caret-bottom el-icon--right"></i>
-          </span>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item class="clearfix">
-              <router-link style="text-decoration:none" :to="{path:'replyedAnswerList',query:{user_id: this.$store.state.user.userId}}">回答</router-link>
-              <!--a href="" style="text-decoration:none">回答</a-->
-              <el-badge class="mark" :value="this.notReadAnswer" />
-            </el-dropdown-item>
-            <el-dropdown-item class="clearfix">
-              <!--a href="" style="text-decoration:none">评论</a-->
-              <router-link style="text-decoration:none" :to="{path:'replyedCommentList',query:{user_id: this.$store.state.user.userId}}">评论</router-link>
-              <el-badge class="mark" :value="this.notReadComment" />
-            </el-dropdown-item>
-            <el-dropdown-item class="clearfix">
-              <!--a href="" style="text-decoration:none">评论</a-->
-              <router-link style="text-decoration:none" :to="{path:'reportedQuestionList',query:{user_id: this.$store.state.user.userId}}">举报</router-link>
-              <el-badge class="mark" :value="this.notReadReport" />
-            </el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-          <router-link style="color: #ffffff;text-decoration:none" :to="{name:'main'}">首页</router-link>
-        /
-        <a id="logout_link1" href="/" @click="logout" style="color: white;text-decoration:none">登出</a>
-      </div>
+          <el-dropdown trigger="click" style="color: white;margin-right: 10px;font-size: 16px">
+            <span class="el-dropdown-link">
+              消息
+              <i class="el-icon-caret-bottom el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item class="clearfix">
+                <router-link style="text-decoration:none" :to="{path:'replyedAnswerList',query:{user_id: this.$store.state.user.userId}}">回答</router-link>
+                <!--a href="" style="text-decoration:none">回答</a-->
+                <el-badge class="mark" :value="this.notReadAnswer" />
+              </el-dropdown-item>
+              <el-dropdown-item class="clearfix">
+                <!--a href="" style="text-decoration:none">评论</a-->
+                <router-link style="text-decoration:none" :to="{path:'replyedCommentList',query:{user_id: this.$store.state.user.userId}}">评论</router-link>
+                <el-badge class="mark" :value="this.notReadComment" />
+              </el-dropdown-item>
+              <el-dropdown-item class="clearfix">
+                <!--a href="" style="text-decoration:none">评论</a-->
+                <router-link style="text-decoration:none" :to="{path:'reportedQuestionList',query:{user_id: this.$store.state.user.userId}}">举报</router-link>
+                <el-badge class="mark" :value="this.notReadReport" />
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+            <router-link style="color: #ffffff;text-decoration:none" :to="{name:'main'}">首页</router-link>
+          /
+          <a id="logout_link1" href="/" @click="logout" style="color: white;text-decoration:none">登出</a>
+        </div>
         <div v-else-if="admin" style="position: absolute;right: 15px;top: -15px ">
           <!--router-link style="color:white;text-decoration:none;margin-right:20px" :to="{name:'newquestionPage',query:{user_id:admin.adminId}}">我要提问</router-link-->
           <!--span style="margin-right: 5px">{{admin.name}}</span-->
@@ -98,6 +98,76 @@
         brListData:[],
         testrListData:[],
       }
+    },
+    created() {
+      //在页面刷新时将vuex里的信息保存到sessionStorage里
+      window.addEventListener("beforeunload", () => {
+        console.log("存vuex前的数据")
+        console.log(this.$store.state,JSON.parse(sessionStorage.getItem("store")))
+
+        sessionStorage.setItem("store", JSON.stringify(this.$store.state))
+
+        console.log("存vuex后的数据")
+        console.log(this.$store.state,JSON.parse(sessionStorage.getItem("store")))
+      })
+      // 在页面加载时读取sessionStorage里的状态信息
+      if (sessionStorage.getItem("store")) {
+        console.log("读取sessionstorage前的数据")
+        console.log(this.$store.state,JSON.parse(sessionStorage.getItem("store")))
+
+        this.$store.replaceState(Object.assign({}, this.$store.state, JSON.parse(sessionStorage.getItem("store"))))
+
+        console.log("读取sessionstorage后的数据")
+        console.log(this.$store.state,JSON.parse(sessionStorage.getItem("store")))
+      }
+      this.getaListData()
+      //alert("AAA")
+      this.getcListData()
+      this.getrListData()
+    },
+    computed: {
+      user () {
+        return this.$store.state.user
+      },
+      admin() {
+        return this.$store.state.admin
+      },
+      imageUrl(){
+        if(this.user){
+          console.log("现在是用户登录状态")
+          return "http://localhost:8080"+this.$store.state.user.image
+        }
+        else if(this.admin){
+          console.log("现在是管理员登录状态")
+          console.log("管理员头像:",this.$store.state.admin)
+          return "http://localhost:8080"+this.$store.state.admin.image
+        }
+      },
+      notReadAnswer(){
+        if(this.user){
+          //this.getaListData()
+          this.getaListData()
+          console.log("记录未读回答数",this.testaListData.length)
+          //alert("AAA")
+          return this.testaListData.length
+        }
+      },
+      notReadComment(){
+        if(this.user){
+          //this.getaListData()
+          this.getcListData()
+          console.log("记录未读评论数",this.testcListData.length)
+          return this.testcListData.length
+        }
+      },
+      notReadReport(){
+        if(this.user){
+          //this.getaListData()
+          this.getrListData()
+          console.log("记录未读被举报数",this.testrListData.length)
+          return this.testrListData.length
+        }
+      },
     },
     methods:{
       logout(){
@@ -213,77 +283,12 @@
               }
             }
             console.log("this.testrListData是",this.testrListData)
-
         }).catch((error) => {
           console.log(error);
         });
       },
     },
-    created() {
-      //在页面刷新时将vuex里的信息保存到sessionStorage里
-      window.addEventListener("beforeunload", () => {
-        console.log("存vuex前的数据")
-        console.log(this.$store.state,JSON.parse(sessionStorage.getItem("store")))
 
-        sessionStorage.setItem("store", JSON.stringify(this.$store.state))
-
-        console.log("存vuex后的数据")
-        console.log(this.$store.state,JSON.parse(sessionStorage.getItem("store")))
-      })
-      // 在页面加载时读取sessionStorage里的状态信息
-      if (sessionStorage.getItem("store")) {
-        console.log("读取sessionstorage前的数据")
-        console.log(this.$store.state,JSON.parse(sessionStorage.getItem("store")))
-
-        this.$store.replaceState(Object.assign({}, this.$store.state, JSON.parse(sessionStorage.getItem("store"))))
-
-        console.log("读取sessionstorage后的数据")
-        console.log(this.$store.state,JSON.parse(sessionStorage.getItem("store")))
-      }
-      this.getaListData()
-      this.getcListData()
-      this.getrListData()
-    },
-    computed: {
-      user () {
-        return this.$store.state.user
-      },
-      admin() {
-        return this.$store.state.admin
-      },
-      imageUrl(){
-        if(this.user){
-          console.log("现在是用户登录状态")
-          return "http://localhost:8080"+this.$store.state.user.image
-        }
-        else if(this.admin){
-          console.log("现在是管理员登录状态")
-          console.log("管理员头像:",this.$store.state.admin)
-          return "http://localhost:8080"+this.$store.state.admin.image
-        }
-      },
-      notReadAnswer(){
-        if(this.user){
-          //this.getaListData()
-          console.log("记录未读回答数",this.testaListData.length)
-          return this.testaListData.length
-        }
-      },
-      notReadComment(){
-        if(this.user){
-          //this.getaListData()
-          console.log("记录未读评论数",this.testcListData.length)
-          return this.testcListData.length
-        }
-      },
-      notReadReport(){
-        if(this.user){
-          //this.getaListData()
-          console.log("记录未读被举报数",this.testrListData.length)
-          return this.testrListData.length
-        }
-      },
-    },
   }
 </script>
 
